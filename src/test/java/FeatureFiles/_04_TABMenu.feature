29e747f101a324b2013066c81e438993fe1bbea4
sefacan <sefacanbasoglu67@gmail.com>
2024-10-31 14:42:56 +0300

    us 4

---
+++ src/test/java/FeatureFiles/_04_TABMenu.feature
@@ -0,0 +1,9 @@
+Feature: TabMenu Functionality
+
+  Scenario:  TAB menu is displayed
+    Given Navigate to the website
+    When Click on the  sing in button
+    When Enter userName and Password and click sing in button
+    When Clicking on each main category should display relevant subcategories.
+    When Clicking on each main category should display the relevant subcategories and displayed Tops and Bottoms
+    When Clicking on the Tops and Bottoms subcategories should display the relevant product subcategories.
---
+++ src/test/java/StepDefinitions/TabMenu.java
@@ -0,0 +1,48 @@
+package StepDefinitions;
+
+import Pages.Elements;
+import Utilities.GWD;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+import java.sql.Driver;
+
+public class TabMenu {
+    Elements elements=new Elements();
+    Actions actions=new Actions(GWD.getDriver());
+    @When("Clicking on each main category should display relevant subcategories.")
+    public void clickingOnEachMainCategoryShouldDisplayRelevantSubcategories() {
+        Assert.assertTrue(elements.WhatsNew4.isDisplayed(),"Whats new cannot be displayed");
+        Assert.assertTrue(elements.Women4.isDisplayed(),"Women cannot be displayed");
+        Assert.assertTrue(elements.Men4.isDisplayed(),"Men cannot be displayed");
+        Assert.assertTrue(elements.Gear4.isDisplayed(),"Gear cannot be displayed");
+        Assert.assertTrue(elements.Training4.isDisplayed(),"Training cannot be displayed");
+        Assert.assertTrue(elements.Sale4.isDisplayed(),"Sale cannot be displayed");
+    }
+    @When("Clicking on each main category should display the relevant subcategories and displayed Tops and Bottoms")
+    public void clickingOnEachMainCategoryShouldDisplayTheRelevantSubcategoriesAndDisplayedTopsAndBottoms() {
+        actions.moveToElement(elements.Women4).build().perform();
+        Assert.assertTrue(elements.TopsWomen4.isDisplayed(),"TopsWomen cannot be displayed");
+        Assert.assertTrue(elements.BottomsWomen4.isDisplayed(),"BottomsWomen cannot be displayed");
+        actions.moveToElement(elements.Men4).build().perform();
+        Assert.assertTrue(elements.TopsMen4.isDisplayed(),"TopsMen cannot be displayed");
+        Assert.assertTrue(elements.BottomsMen4.isDisplayed(),"BottomsMen cannot be displayed");
+        actions.moveToElement(elements.Gear4).build().perform();
+        actions.moveToElement(elements.Training4).build().perform();
+    }
+
+
+    @When("Clicking on the Tops and Bottoms subcategories should display the relevant product subcategories.")
+    public void clickingOnTheTopsAndBottomsSubcategoriesShouldDisplayTheRelevantProductSubcategories() {
+        actions.moveToElement(elements.Women4).build().perform();
+        actions.moveToElement(elements.TopsWomenMenu4).build().perform();
+        actions.moveToElement(elements.BottomsWomenMenu4).build().perform();
+        actions.moveToElement(elements.Men4).build().perform();
+        actions.moveToElement(elements.TopsMenMenu4).build().perform();
+
+        actions.moveToElement(elements.BottomsMenMenu4).build().perform();
+    }
+
+
+}
